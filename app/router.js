482c894045c726374440a4fc37506013cefb2efa
yassir-abid <abid.yassir@gmail.com>
2022-08-25 10:57:03 +0200

    middleware to catch error 404

--- app/router.js
+++ app/router.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
-const { errorHandler } = require('./helpers/errorHandler');
+const { errorHandler, ApiError } = require('./helpers/errorHandler');
 
 const mainController = require('./controllers');
 
 const router = express.Router();
 
 router.get('/test', mainController.test);
+
+router.use(() => {
+    throw new ApiError('Page not found', { statusCode: 404 });
+});
 
 router.use((err, _, response, next) => {
     errorHandler(err, response, next);

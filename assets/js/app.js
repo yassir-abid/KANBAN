e0d3a3cbef8f216bd215f46777294f6d56dedbf8
yassir-abid <abid.yassir@gmail.com>
2022-08-29 12:14:15 +0200

    display lists in DOM using template content

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,22 @@ const app = {
         try {
             const response = await fetch(`${app.base_url}/lists`);
             const lists = await response.json();
-            console.log(lists);
+            lists.forEach((list) => {
+                app.makeListInDOM(list);
+            });
         } catch (error) {
             console.error(error);
         }
+    },
+
+    makeListInDOM: (list) => {
+        const template = document.getElementById('listTemplate');
+        const clone = document.importNode(template.content, true);
+
+        clone.querySelector('h2').textContent = list.title;
+        clone.querySelector('.panel').dataset.listId = list.id;
+
+        document.querySelector('.card-lists').appendChild(clone);
     },
 
 };

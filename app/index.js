e0fb8e69232942e5122549acdd63632924e793d8
yassir-abid <abid.yassir@gmail.com>
2022-09-20 20:56:19 +0200

    add session middleware using express-session package

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const multer = require('multer');
 
@@ -7,6 +8,16 @@ const bodyParser = multer();
 const router = require('./router');
 
 const app = express();
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: (1000 * 60 * 60),
+    },
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
